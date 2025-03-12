c513051e6a3462c52de744389fb82367dd431dc7
Esther-Mokwena <nmokwenaesther@gmail.com>
2025-03-12 18:21:58 +0200

    tried adding API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
+function updateWeatherData(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let weatherInfoElement = document.querySelector(".weather-info");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  weatherInfoElement.innerHTML = `Today, ${response.data.time} | ${response.data.condition.description}
+    <br />
+    Humidity: <strong>${response.data.temperature.humidity}%</strong> , Wind: <strong> ${response.data.wind.speed}km/h</strong>`;
+}
+
+function searchCity(city) {
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(updateWeatherData);
+}
+
 function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
-  }
-  
-  let searchFormElement = document.querySelector("#search-form");
-  searchFormElement.addEventListener("submit", handleSearchSubmit);
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Pretoria");
+
+
